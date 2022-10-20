e755067f930c8908ec474ce60b57459cb06c402a
jihyunglee <jhlee7125@gmail.com>
2022-10-20 20:47:25 +0900

    [add] 03 Reviews - 구현 완료

--- 03-reviews/app.js
+++ 03-reviews/app.js
@@ -29,3 +29,57 @@ const reviews = [
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
+
+// Select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// set starting item
+let currentItem = 0;
+
+// laad initial item
+window.addEventListener('DOMContentLoaded', () => {
+  // console.log('shake and bake');
+  showPerson();
+});
+
+// show person based on item
+function showPerson() {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener('click', () => {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  // console.log(currentItem);
+  showPerson();
+});
+
+// show prev person
+prevBtn.addEventListener('click', () => {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  // console.log(currentItem);
+  showPerson();
+});
+
+// show random person
+randomBtn.addEventListener('click', () => {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson();
+});
