065ccde74cb5f6f495bc8007ae44ec2ed4764f5e
jihyunglee <jhlee7125@gmail.com>
2022-10-20 17:10:54 +0900

    02 Counter - NodeList API인 forEach() 미지원 브라우저에 대한 대책 사항인 Array.prototype.forEach.call() 구현 적용

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -6,9 +6,39 @@ const value = document.getElementById('value');
 const btns = document.querySelectorAll('.btn');
 
 // This "forEach" method is one of the NodeList.APIs
+
+// btns.forEach((btn) => {
+//   btn.addEventListener('click', (e) => {
+//     // console.log(e.currentTarget.classList);
+//     const styles = e.currentTarget.classList;
+//     /* contains()는 포함 여부를 판단하여 true, false를 반환한다 */
+//     if (styles.contains('decrease')) {
+//       count--;
+//     } else if (styles.contains('increase')) {
+//       count++;
+//     } else {
+//       count = 0;
+//     }
+//     /* positive, zero, negative에 따라 value의 색상 정보를 변경 */
+//     if (count > 0) {
+//       value.style.color = 'limegreen';
+//     } else if (count < 0) {
+//       value.style.color = 'coral';
+//     }
+//     /* span 요소에 count 값을 텍스트로 넣는다 */
+//     value.textContent = count;
+//   });
+// });
+
 // Not Array.prototype!
-// But you can make it by using call()
-btns.forEach((btn) => {
+// But you can make it by using
+/*
+items = document.querySelectorAll('items');
+Array.prototype.forEach.call(this(=items), (item) => {
+  item.addEventListener('click', () => {});
+}
+*/
+Array.prototype.forEach.call(btns, (btn) => {
   btn.addEventListener('click', (e) => {
     // console.log(e.currentTarget.classList);
     const styles = e.currentTarget.classList;
