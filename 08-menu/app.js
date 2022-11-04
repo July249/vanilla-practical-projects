0a4fa2ed76bdcf043c28d484572860bf7037d854
jihyunglee <jhlee7125@gmail.com>
2022-11-04 11:49:55 +0900

    [add] 08 Menu - displayMenuItems 함수 구현

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -72,3 +72,36 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+/* id, title, category, price, img, desc */
+
+const container = document.querySelector('.btn-container');
+const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+// load items
+window.addEventListener('DOMContentLoaded', () => {
+  displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map((item) => {
+    return `
+      <article class="menu-item">
+      <img src="${item.img}" alt="menu item" class="photo" />
+      <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">$${item.price}</h4>
+        </header>
+        <p class="item-text">
+          ${item.desc}
+        </p>
+      </div>
+    </article>
+    `;
+  });
+
+  displayMenu = displayMenu.join('');
+
+  sectionCenter.innerHTML = displayMenu;
+}
